bc10f70c8081f7464a3f0aff5e2be4b3de5f400d
Shira Zadok <shira19941@gmail.com>
2021-08-27 01:44:17 +0300

    Add files via upload

--- Create_Uplad_CopyBlobs.ps1
+++ Create_Uplad_CopyBlobs.ps1
@@ -1,4 +1,9 @@
-        $ResourceGroupName= "ShiraStorageAccount-rg"
+        #Make sure that I run this command against the VMs in the right Subscription
+		$SubscriptionID= "a8108c2b-496c-424d-8347-ecc8afb6384c"
+		
+		Set-AzContext -Subscription $SubscriptionID
+		
+		$ResourceGroupName= "ShiraStorageAccount-rg"
         $srcStorageAccountName = "mysorage11"
         $srcStorageAccountContext = (Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $srcStorageAccountName).Context
         $destStorageAccountContext = (Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $destStorageAccountName).Context
