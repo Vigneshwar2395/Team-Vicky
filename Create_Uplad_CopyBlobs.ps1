b45a84adcd18584ac3511e95e602dac7ab32360b
Shira Zadok <shira19941@gmail.com>
2021-08-27 03:22:51 +0300

    Update Create_Uplad_CopyBlobs.ps1

--- Create_Uplad_CopyBlobs.ps1
+++ Create_Uplad_CopyBlobs.ps1
@@ -1,7 +1,7 @@
         #Make sure that I run this command against the VMs in the right Subscription
-	$SubscriptionID= "a8108c2b-496c-424d-8347-ecc8afb6384c"
+	#$SubscriptionID= "a8108c2b-496c-424d-8347-ecc8afb6384c"
 		
-	Set-AzContext -Subscription $SubscriptionID
+	#Set-AzContext -Subscription $SubscriptionID
 		
 	$ResourceGroupName= "ShiraStorageAccount-rg"
         $srcStorageAccountName = "mysorage11"
