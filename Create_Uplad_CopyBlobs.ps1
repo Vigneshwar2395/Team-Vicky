39e3bbc46872b8fd7abfe09e1d2f7dab6e74d680
Shira Zadok <shira19941@gmail.com>
2021-08-29 15:51:52 +0300

    Update Create_Uplad_CopyBlobs.ps1

--- Create_Uplad_CopyBlobs.ps1
+++ Create_Uplad_CopyBlobs.ps1
@@ -11,8 +11,26 @@ $srcContainer = "sourcecontainer"
 $destContainer = "destcontainer"
 
 #Create source and destination container
-New-AzStorageContainer -Name $srcContainer -Context $srcStorageAccountContext -Permission blob
-New-AzStorageContainer -Name $destContainer -Context $destStorageAccountContext -Permission blob
+Try
+{
+   $containerSrcCheck = Get-AzStorageContainer -Context $srcStorageAccountContext
+   $containerDestCheck = Get-AzStorageContainer -Context $destStorageAccountContext
+}
+Catch
+{
+    # Catch any error
+    #The containers are exist
+}
+Finally
+{
+    if($containerSrcCheck -eq $null){Write-Output "This is null"}
+    if($containerDestCheck -eq $null){Write-Output "This is alsooooo null"}
+
+    if(($containerSrcCheck -eq $null) -and ($containerDestCheck -eq $null)){
+        New-AzStorageContainer -Name $srcContainer -Context $srcStorageAccountContext -Permission blob
+        New-AzStorageContainer -Name $destContainer -Context $destStorageAccountContext -Permission blob
+    }
+}
         
 $srcStorageKey = Get-AzStorageAccountKey -Name $srcStorageAccountName `
 -ResourceGroupName $ResourceGroupName 
@@ -21,10 +39,8 @@ $srcContext = New-AzStorageContext -StorageAccountName $srcStorageAccountName `
 #Upload 100 blobs
 Get-ChildItem -Path C:\Users\shira.zadok\Desktop\100Blobs | Set-AzStorageBlobContent -Container $srcContainer `
 -Context $srcContext -Force
-
 #$sourcecontainerContext = New-AzStorageContainerSASToken -Context $srcStorageAccountContext -Name sourcecontainer -Permission racwdl -FullUri
 #$destcontainerContext = New-AzStorageContainerSASToken -Context $destStorageAccountContext -Name destcontainer -Permission racwdl -FullUri
-
 #Copy 100 blob
 #azcopy copy $sourcecontainerContext $destcontainerContext --recursive
-azcopy copy "https://shirastorageaccount0a.blob.core.windows.net/sourcecontainer?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacuptfx&se=2021-08-29T12:13:44Z&st=2021-08-29T04:13:44Z&spr=https&sig=OW6fwDHxRYEqEizyn4oavl5VzdukDcBwpmZwg1KuVM8%3D" "https://shirastorageaccount0b.blob.core.windows.net/destcontainer?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacuptfx&se=2021-10-31T13:15:00Z&st=2021-08-29T04:15:00Z&spr=https&sig=wc7Cdywu2dQ15yqoMEMm9Khhy%2B%2F1AgytRZhZkPKN34U%3D" --recursive
+azcopy copy "https://shirastorageaccount0a.blob.core.windows.net/sourcecontainer?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacuptfx&se=2021-08-29T20:27:37Z&st=2021-08-29T12:27:37Z&spr=https&sig=iA%2FgcL8U55D2J%2FFGyichLvNX572KfvlKktizV6rZl4I%3D" "https://shirastorageaccount0b.blob.core.windows.net/destcontainer?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacuptfx&se=2021-10-31T21:25:13Z&st=2021-08-29T12:25:13Z&spr=https&sig=G1iul%2Btlgy78yboQlfBMd7XyLJZTqsLA5DVHr8BnXF0%3D" --recursive
