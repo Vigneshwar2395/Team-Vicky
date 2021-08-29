208c21efe956fcd46e9a4a986abbf70b39dce265
Shira Zadok <shira19941@gmail.com>
2021-08-29 00:42:03 +0000

    Add files via upload

---
+++ Create_Uplad_CopyBlobs.ps1
@@ -0,0 +1,27 @@
+#Make sure that I run this command against the VMs in the right Subscription
+$SubscriptionID= "a8108c2b-496c-424d-8347-ecc8afb6384c"
+		
+Set-AzContext -Subscription $SubscriptionID
+	
+$ResourceGroupName= "ShiraStorageAccount-rg"
+$srcStorageAccountName = "shirastorageaccount0a"
+$destStorageAccountName = "shirastorageaccount0b"
+$srcStorageAccountContext = (Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $srcStorageAccountName).Context
+$destStorageAccountContext = (Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $destStorageAccountName).Context
+$srcContainer = "sourcecontainer"
+$destContainer = "destcontainer"
+
+#Create source and destination container
+New-AzStorageContainer -Name $srcContainer -Context $srcStorageAccountContext -Permission blob
+New-AzStorageContainer -Name $destContainer -Context $destStorageAccountContext -Permission blob
+        
+$srcStorageKey = Get-AzStorageAccountKey -Name $srcStorageAccountName `
+                 -ResourceGroupName $ResourceGroupName 
+$srcContext = New-AzStorageContext -StorageAccountName $srcStorageAccountName `
+              -StorageAccountKey $srcStorageKey.Value[0] 
+#Upload 100 blobs
+Get-ChildItem -Path C:\Users\shira.zadok\Desktop\100Blobs | Set-AzStorageBlobContent -Container $srcContainer `
+-Context $srcContext -Force
+
+#Copy 100 blob
+azcopy copy "https://shirastorageaccount0a.blob.core.windows.net/sourcecontainer?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacuptfx&se=2021-08-29T08:38:04Z&st=2021-08-29T00:38:04Z&spr=https&sig=YdByD9ZcZchT%2FgwedbfIsUAsJq0kdF%2BNdKDCcy2P9ss%3D" "https://shirastorageaccount0b.blob.core.windows.net/destcontainer?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacuptfx&se=2021-10-31T09:35:18Z&st=2021-08-29T00:35:18Z&spr=https&sig=E67fA086kA9Vi3DQ4ytcL81%2BBNiRRSNO3D1xLAkLyqE%3D" --recursive
