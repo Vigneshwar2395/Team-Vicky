91721e66606fcb74de298792c8610af497ddb23f
Shira Zadok <shira19941@gmail.com>
2021-08-29 07:26:41 +0300

    Update Create_Uplad_CopyBlobs.ps1

--- Create_Uplad_CopyBlobs.ps1
+++ Create_Uplad_CopyBlobs.ps1
@@ -1,6 +1,5 @@
 #Make sure that I run this command against the VMs in the right Subscription
-$SubscriptionID= "a8108c2b-496c-424d-8347-ecc8afb6384c"
-		
+$SubscriptionID= "a8108c2b-496c-424d-8347-ecc8afb6384c"		
 Set-AzContext -Subscription $SubscriptionID
 	
 $ResourceGroupName= "ShiraStorageAccount-rg"
@@ -10,9 +9,10 @@ $srcStorageAccountContext = (Get-AzStorageAccount -ResourceGroupName $ResourceGr
 $destStorageAccountContext = (Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $destStorageAccountName).Context
 $srcContainer = "sourcecontainer"
 $destContainer = "destcontainer"
+
 #Create source and destination container
-#New-AzStorageContainer -Name $srcContainer -Context $srcStorageAccountContext -Permission blob
-#New-AzStorageContainer -Name $destContainer -Context $destStorageAccountContext -Permission blob
+New-AzStorageContainer -Name $srcContainer -Context $srcStorageAccountContext -Permission blob
+New-AzStorageContainer -Name $destContainer -Context $destStorageAccountContext -Permission blob
         
 $srcStorageKey = Get-AzStorageAccountKey -Name $srcStorageAccountName `
                  -ResourceGroupName $ResourceGroupName 
@@ -22,5 +22,9 @@ $srcContext = New-AzStorageContext -StorageAccountName $srcStorageAccountName `
 Get-ChildItem -Path C:\Users\shira.zadok\Desktop\100Blobs | Set-AzStorageBlobContent -Container $srcContainer `
 -Context $srcContext -Force
 
+#$sourcecontainerContext = New-AzStorageContainerSASToken -Context $srcStorageAccountContext -Name sourcecontainer -Permission racwdl -FullUri
+#$destcontainerContext = New-AzStorageContainerSASToken -Context $destStorageAccountContext -Name destcontainer -Permission racwdl -FullUri
+
 #Copy 100 blob
-azcopy copy "https://shirastorageaccount0a.blob.core.windows.net/sourcecontainer?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacuptfx&se=2021-10-31T10:03:46Z&st=2021-08-29T01:03:46Z&spr=https&sig=dAXc8MNHpgNnrAbiqtGM7GGMdLpoixmUKfyKSBatWZc%3D" "https://shirastorageaccount0b.blob.core.windows.net/destcontainer?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacuptfx&se=2021-10-31T10:02:16Z&st=2021-08-29T01:02:16Z&spr=https&sig=ik5acirjGtqsFl%2Fn566Nx5ntLh3BTMY8VCi60j6iqFA%3D" --recursive
+#azcopy copy $sourcecontainerContext $destcontainerContext --recursive
+azcopy copy "https://shirastorageaccount0a.blob.core.windows.net/sourcecontainer?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacuptfx&se=2021-08-29T12:13:44Z&st=2021-08-29T04:13:44Z&spr=https&sig=OW6fwDHxRYEqEizyn4oavl5VzdukDcBwpmZwg1KuVM8%3D" "https://shirastorageaccount0b.blob.core.windows.net/destcontainer?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacuptfx&se=2021-10-31T13:15:00Z&st=2021-08-29T04:15:00Z&spr=https&sig=wc7Cdywu2dQ15yqoMEMm9Khhy%2B%2F1AgytRZhZkPKN34U%3D" --recursive
